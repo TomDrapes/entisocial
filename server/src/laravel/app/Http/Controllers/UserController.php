128497fc1af1d20462c4d23a6e82416eadbb66db
Tom Drapes <tjdrapes@gmail.com>
2020-06-05 22:58:04 +1000

    update User controller

---
+++ server/src/laravel/app/Http/Controllers/UserController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function createUser(Request $request)
+    {
+        echo 'OI CUNT';
+
+        $request->validate(
+            [
+                'firstName'     => 'required',
+                'lastName'     => 'required',
+                'email'    => 'required',
+                'password' => 'required'
+            ]
+        );
+
+         $user = new User(
+            [
+                'firstName'     => $request->get('firstName'),
+                'lastName'     => $request->get('lastName'),
+                'email'    => $request->get('email'),
+                'password' => $request->get('password'),
+
+            ]
+         );
+
+        $user->save();
+
+        return response()->json([
+            'message' => 'new user created'
+                                ], 201);
+    }
+
+    public function getUser()
+    {
+        $user = User::get()->toJson(JSON_PRETTY_PRINT);
+        return response($user, 200);
+    }
+
+    public function updateUser()
+    {
+    }
+
+    public function deleteUser()
+    {
+    }
+}
