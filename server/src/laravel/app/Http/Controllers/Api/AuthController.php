6b43fc5a9edb89948d27e6fe4c37fc402d826612
Tom Drapes <tjdrapes@gmail.com>
2020-06-27 11:06:52 +1000

    removed auto generated login and register controllers and removed cors middleware due to conflicts with nginx headers

--- server/src/laravel/app/Http/Controllers/Api/AuthController.php
+++ server/src/laravel/app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Client;
-use GuzzleHttp\Exception\ClientException;
+use App\Http\Controllers\MailController;
+use App\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
@@ -27,5 +29,56 @@ class AuthController extends Controller
         }
 
         return response()->json(['message' => 'fail']);
+    }
+
+    /**
+     * Create a new user and send email verification
+     *
+     * @param Request $request
+     *
+     * @return JsonResponse
+     */
+    public function registerUser(Request $request)
+    {
+        $user = new User();
+        $user->firstName = $request->get('firstName');
+        $user->lastName = $request->get('lastName');
+        $user->email = $request->get('email');
+        $user->password = Hash::make($request->get('password'));
+        $user->verification_code = sha1(time());
+        $user->save();
+
+        if ($user !== null) {
+            echo 'Send verification email';
+            //Send verification email
+            MailController::sendSignupEmail($user->firstName, $user->email, $user->verification_code);
+
+            //Notify user
+            return response()->json(
+                [
+                    'message' => 'Verification email sent'
+                ],
+                200
+            );
+        }
+
+        return response()->json(
+            [
+                'message' => 'Registration failed'
+            ]
+        );
+    }
+
+    public function verifyUser(Request $request)
+    {
+        $verification_code = $request->get('code');
+        $user = User::where(['verification_code' => $verification_code])->first();
+        if ($user !== null) {
+            $user->is_verified = 1;
+            $user->save();
+            MailController::sendWelcomeEmail($user->email, $user->password);
+            return view('auth.verify-success');
+        }
+
     }
 }
--- server/src/laravel/routes/api.php
+++ server/src/laravel/routes/api.php
@@ -28,13 +28,7 @@ use Illuminate\Support\Facades\Route;
      Route::delete('cords/{cord}', 'CordController@deleteCord');
  });
 
- /* Login */
+ /* Public Auth Routes */
 Route::post('login', 'Api\AuthController@login');
-
-/* Registration Routes */
-Route::post('register', 'Auth\RegisterController@registerUser');
-Route::get('verify', 'Auth\RegisterController@verifyUser');
-
-
-
-//eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI5MGQ5M2JmYS0yYTkxLTQ4MjctYWQ2ZS0zOTkxM2U3ZTU2MWIiLCJqdGkiOiIwMDUyNzc2YTYzYzcxYzkyMzI3YjlkMGQ3ZTNlZWFiYjkyZjZlMGJjYmI4NTk0OGMzNjkwZjY1ODdlMGI0MDg5NmIwZWNhYTQ5ZjcyZjU3NiIsImlhdCI6MTU5MjYyNzI1MSwibmJmIjoxNTkyNjI3MjUxLCJleHAiOjE2MjQxNjMyNTEsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.g_0qmUnfihahHlDt3B60mUbiGTOwNMk1mQDaqBIewDbFtB_5jViJ1jWc3jprQSt85V5pa-ttjSTJu3ijgyK-UnGHFcMOW3H1njdOLLUlXXNAvxjlj4jzAmJIT1hNuC4pM858agFz-USQHxueS5P700gALsZX9Jl38JI3S0Z2BeBUKOM2cjJAezZRzsjWkuZ6HseOd54nu-5Iw4_GXx34ifAq0LxQEWxpwv1_eLNHgRpQ84ZJZfdE1_3gu4gH_kiYqzh09BuE0FgXbWs_A_MaG5lPlYkhhE6AhL0-yn2WvmNjX773QUpMK2I9mUvzsdJZ9MQnd8w8JPH1fzHe-61RPxxFEkobapnZ0J2BP9mi4QfaRD6iX7NRe1TPrm5edn6GxCHB9pbrAM3LCeJeceqOMq5ZeXP2NJH46WVew9o2Y2W9fRjcoymKsXQ-W3hbJZ-SXbrzR-5kN1iLn0A8lT84bfVWBxMET7RpA0aOCkY-SEg-eVMg2xHyZ9UPyYGOY8W-npkRO-JQM3_VhXrVl0kyWMH0ct9sX3Dqf4nlc02j_BugFQ4yx1TRVQdKtAUrWfi59bx27Q6RSLkL431Es_NiRzNvkxD2URlV64ZEi-2031j3kn9ffl6Ms0ruFuvc6_0bkdabuBbhL5qxRVjzhAbTnzxcpEe8LQWR_LJrzbGd1Dc
+Route::post('register', 'Api\AuthController@registerUser');
+Route::get('verify', 'Api\AuthController@verifyUser');
